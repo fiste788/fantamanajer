0845fbe74d42d7ef086ecdfbf58e60f2c641554c
Fiste788 <stefano788@gmail.com>
2008-04-24 18:34:24 +0000

    git-svn-id: http://subversion.assembla.com/svn/fantamanajer@8 37f4913f-0a8a-4499-b109-c3fdd495100e

--- inc/auth.inc.php
+++ inc/auth.inc.php
@@ -8,7 +8,7 @@ function login($username, $password)
 	$q = "SELECT username, password FROM squadra WHERE username LIKE '".$username."';";
 	$exe = mysql_query($q) or die(MYSQL_ERRNO()." ".MYSQL_ERROR() . $q);
 	$valore  = mysql_fetch_row($exe);
-	if(($valore[0] == $username)&&($valore[1] == md5($password)))
+	if((strcasecmp($valore[0],$username) == 0 )&&($valore[1] == md5($password)))
 		return TRUE;
 	else
 		return FALSE;
