b80b150879e3d00cfbb331c06983289b877e3f1a
Dirtymagic <francesco.bertocchi@gmail.com>
2008-08-13 12:26:46 +0000

    Rivoluzionata e semplificata la funzione calcolaPunti...ora manca quella relativa al dettagliogiornata
    git-svn-id: http://subversion.assembla.com/svn/fantamanajer@43 37f4913f-0a8a-4499-b109-c3fdd495100e

--- Cekkazzo/StartScarico.php
+++ Cekkazzo/StartScarico.php
@@ -5,7 +5,8 @@ mysql_connect("localhost","ingo_fm","banana");
 mysql_select_db("test");
 mysql_query("SET NAMES utf8;") or die();
 mysql_query("SET CHARACTER SET utf8;")or die();
+$puntobj = new punteggi();
+//recupera_voti(38);
+$puntobj->calcolaPunti(38,1,"");
 
-
-recupera_voti(39);
 ?>
