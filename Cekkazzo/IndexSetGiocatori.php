e62c157dcfd3818a7a046d7fb07af3d9145ff5a0
dirtymagic <dirty_magic@hotmail.it>
2008-08-29 22:16:00 +0000

    Risolto bug trasferimenti.code.php che visualizzava sempre i trasferimenti della squadra loggata
    git-svn-id: http://subversion.assembla.com/svn/fantamanajer@55 37f4913f-0a8a-4499-b109-c3fdd495100e

--- Cekkazzo/IndexSetGiocatori.php
+++ Cekkazzo/IndexSetGiocatori.php
@@ -1,18 +1,9 @@
-  <title>Setta Voti Giocatori</title>
-  <form name=azzo method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-  Giornata<input type="text" style="text-align:right"name="nome">
-  <br>
-  <input type="submit" value="calcola punti">
-  </form>
+
 <?php
 
-  include 'SetVotiGiocatori.php';
-
-  $giornata=$_POST['nome'];
-  if(isset($_POST['nome']))
-  { 
-    $giornata=$_POST['nome'];
-    recupera_voti($giornata);
-  }
+  include 'Functions.php';
+  connessione();
+  $percorso="..\docs\giocatori08-09.csv";
+  recupera_nomi($percorso);
 
 ?>
