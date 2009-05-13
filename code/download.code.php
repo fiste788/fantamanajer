56a35474e7ce0e1b6aee3cf7f218b754034d209e
Fiste788 <stefano788@gmail.com>
2009-05-13 16:48:49 +0000

    -aggiunto file password
    git-svn-id: http://subversion.assembla.com/svn/fantamanajer/trunk@206 37f4913f-0a8a-4499-b109-c3fdd495100e

--- code/download.code.php
+++ code/download.code.php
@@ -2,20 +2,32 @@
 require_once(INCDIR.'fileSystem.inc.php');
 include_once(INCDIR."createZip.inc.php");
 define ("TOZIP","all");
-	
+
 $fileObj = new fileSystem();
 
 $filesVoti = $fileObj->getFileIntoFolder(VOTIDIR);
 sort($filesVoti); 
 
-$contenttpl->assign('filesVoti',$filesVoti);
+$contenttpl->assign('filesVoti',$filesVoti);2 
 
-if(isset($_POST['giornata']) && $_POST['giornata'] == TOZIP)
+if(isset($_POST['giornata']) && !empty($_POST['giornata']))
 {
-	$createZip = new createZip();
-	$path = $createZip->createZipFromDir(VOTIDIR,'voti');	
-	$createZip->forceDownload($path,"voti.zip");
-	@unlink($path); 
+	if($_POST['giornata'] == TOZIP)
+	{
+		$createZip = new createZip();
+		$path = $createZip->createZipFromDir(VOTIDIR,'voti');
+		$createZip->forceDownload($path,"voti.zip");
+		@unlink($path);
+	}
+	else
+	{
+		header("Content-type: text/csv");
+		header("Content-Disposition: attachment;filename=" . basename($_POST['giornata']));
+		header("Content-Transfer-Encoding: binary");
+		header("Expires: 0");
+		header("Pragma: no-cache");
+		readfile(VOTIDIR . $_POST['giornata']);
+	}
+	die();
 }
-
 ?>
