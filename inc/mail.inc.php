1d12b9ad8ad72288c06da7db9b07fe755650ae82
Fiste788 <stefano788@gmail.com>
2009-08-21 15:53:23 +0000

    corretto metodo ereg deprecato
    git-svn-id: http://subversion.assembla.com/svn/fantamanajer/trunk@213 37f4913f-0a8a-4499-b109-c3fdd495100e

--- inc/mail.inc.php
+++ inc/mail.inc.php
@@ -3,26 +3,10 @@ class mail
 {
 	function checkEmailAddress($email) 
 	{
-		// First, we check that there's one @ symbol, and that the lengths are right
-		if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) 
-			return false;	// Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
-		// Split it into sections to make life easier
-		$emailArray = explode("@", $email);
-		$localArray = explode(".", $emailArray[0]);
-		for ($i = 0; $i < sizeof($localArray); $i++) 
-			if (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $localArray[$i])) 
-				return false;
-		if (!ereg("^\[?[0-9\.]+\]?$", $emailArray[1])) 
-		{ 
-			// Check if domain is IP. If not, it should be valid domain name
-			$domainArray = explode(".", $emailArray[1]);
-			if (sizeof($domainArray) < 2) 
-				return false; // Not enough parts to domain
-			for ($i = 0; $i < sizeof($domainArray); $i++) 
-				if (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domainArray[$i])) 
-					return false;
-		}
-		return true;
+		if(preg_match('/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/',$email))
+			return true;
+		else
+    		return false; 
 	}
   
 	function sendEmail($email,$body,$object)
