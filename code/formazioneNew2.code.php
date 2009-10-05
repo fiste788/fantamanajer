90380d923d5664fe6d19cfe1f51425d06c2c5f34
Fiste788 <stefano788@gmail.com>
2009-10-05 14:45:50 +0000

    -div draggabili
    git-svn-id: http://subversion.assembla.com/svn/fantamanajer/branches/2.5@232 37f4913f-0a8a-4499-b109-c3fdd495100e

---
+++ code/formazioneNew2.code.php
@@ -0,0 +1,171 @@
+<?php
+require_once(INCDIR."utente.inc.php");
+require_once(INCDIR."formazione.inc.php");
+require_once(INCDIR."eventi.inc.php");
+require_once(INCDIR."giocatore.inc.php");
+
+$utenteObj = new utente();
+$eventiObj = new eventi();
+$formazioneObj = new formazione();
+$giocatoreObj = new giocatore();
+
+$mod = NULL;
+$squadra = NULL;
+if(isset($_POST['squadra']))
+	$squadra = $_POST['squadra'];
+if(isset($_POST['mod']))
+	$mod = $_POST['mod'];
+$contenttpl->assign('squadra',$squadra);
+
+$val = $utenteObj->getElencoSquadre();
+$contenttpl->assign('elencosquadre',$val);
+	
+if(TIMEOUT == FALSE)
+	header("Location: ".$contenttpl->linksObj->getLink('altreFormazioni'));
+
+$formImp = $formazioneObj->getFormazioneExistByGiornata(GIORNATA,$_SESSION['legaView']);
+if(isset($formImp[$_SESSION['idSquadra']]) && (TIMEOUT))
+	unset($formImp[$_SESSION['idSquadra']]);
+$contenttpl->assign('formazioniImpostate',$formImp);
+
+$missing = 0;
+$frega = 0;
+$ruo = array('P','D','C','A');
+$elencocap = array('C','VC','VVC');
+$contenttpl->assign('ruo',$ruo);
+$contenttpl->assign('elencocap',$elencocap);
+if(TIMEOUT)
+{
+	$issetform = $formazioneObj->getFormazioneBySquadraAndGiornata($_SESSION['idSquadra'],GIORNATA);	
+	$giocatori = $giocatoreObj->getGiocatoriByIdSquadra($_SESSION['idSquadra']);
+	$contenttpl->assign('giocatori',$giocatori);
+
+	//CONTROLLO SE LA FORMAZIONE È GIA SETTATA E IN QUEL CASO LO PASSO ALLA TPL PER VISUALIZZARLO NELLE SELECT
+		
+	/* CONTROLLI SULL'INPUT: 
+	I VALORI NON DEVONO ESSERE DOPPI 
+	IL CAPITANO FACOLTATIVO
+	*/
+	if(isset($_POST) && !empty($_POST) && isset($_POST['button']))
+	{
+		$formazione = array();
+		$capitano = array("C" => NULL,"VC" => NULL,"VVC" => NULL);
+		$err = 0;
+		
+		foreach($ruo as $ruolo)
+		{
+			foreach($_POST[$ruolo] as $key=>$val)
+			{
+				if(empty($val))
+				{
+					$missing ++;
+					$err ++;
+				}
+				if( !in_array($val,$formazione))
+					$formazione[] = $val;		
+				else
+					$err++;
+			}
+		}
+		foreach($_POST['panch'] as $key=>$val)
+		{
+			if(!empty($val))
+			{
+				if( !in_array($val,$formazione))
+					$formazione[] = $val;		
+				else
+					$err++;
+			}	
+		}
+		foreach($_POST['cap'] as $key=>$val)
+		{
+			if(!empty($val))
+			{
+				$ruoloGioc = $giocatoreObj->getRuoloByIdGioc($val);
+				if( $ruoloGioc == 'P' || $ruoloGioc == 'D' )
+				{
+					if( !in_array($val,$capitano))
+						$capitano[$key] = $val;		
+					else
+						$err++;
+				}
+				else
+				{
+					$frega++;
+					$err++;
+				}
+			}	
+		}
+		//echo "<pre>".print_r($formazione,1)."</pre>";
+		//echo "<pre>".print_r($capitano,1)."</pre>";
+		if ($err == 0)	//VUOL DIRE CHE NON CI SONO VALORI DOPPI
+		{
+			unset($_POST);
+			if(!$issetform)
+			{
+				$id = $formazioneObj->caricaFormazione($formazione,$capitano,GIORNATA,$_SESSION['idSquadra'],$mod);
+				$eventiObj->addEvento('3',$_SESSION['idSquadra'],$_SESSION['idLega'],$id);
+			}
+			else
+				$id = $formazioneObj->updateFormazione($formazione,$capitano,GIORNATA,$_SESSION['idSquadra'],$mod);
+			$message[0] = 0;
+			$message[1] = 'Formazione caricata correttamente';
+		}
+		else
+		{
+			$message[0] = 1;
+			$message[1] = 'Hai inserito dei valori multipli';
+		}
+		if ($missing > 0)
+		{
+			$message[0] = 1;
+			$message[1] = 'Valori mancanti';
+		}
+		if ($frega > 0)
+		{
+			$message[0] = 1;
+			$message[1] = 'Stai cercando di fregarmi?';
+		}
+		$contenttpl->assign('message',$message);
+	}
+	$issetform = $formazioneObj->getFormazioneBySquadraAndGiornata($_SESSION['idSquadra'],GIORNATA);	
+	if($issetform)
+	{
+		if(empty($_POST))
+			$mod = $issetform['modulo'];
+		$panchinariAr = $issetform['elenco'];
+		$titolariAr = array_splice($panchinariAr,0,11);
+		$i = 0;
+		if(!empty($_POST))
+		{
+			foreach($ruo as $ruolo)
+			{
+				foreach($_POST[$ruolo] as $key=>$val)
+				{
+					$titolariAr[$i] = $val;
+					$i++;
+				}
+			}
+			foreach($_POST['panch'] as $key=>$val)
+			{
+				$panchinariAr[$i] = $val;
+				$i++;
+			}
+			foreach($_POST['cap'] as $key=>$val)
+				$capitano[$key] = $val;
+		}
+		$contenttpl->assign('titolari',$titolariAr);
+		if(empty($panchinariAr))
+			$contenttpl->assign('panchinari',FALSE);
+		else
+			$contenttpl->assign('panchinari',$panchinariAr);
+		$contenttpl->assign('cap',$issetform['cap']);
+	}
+	$contenttpl->assign('issetForm',$issetform);
+	$contenttpl->assign('mod',$mod);
+	if($mod != NULL)
+		$contenttpl->assign('modulo',explode('-',$mod));
+	else
+		$contenttpl->assign('modulo',NULL);
+}
+?>
---
+++ tpl/formazioneNew2.tpl.php
@@ -0,0 +1,334 @@
+<?php $j =0; $k = 0; ?>
+<?php print_r($this->titolari) ?>
+<div id="formazione" class="main-content" style="position:relative;">
+	<?php  if(TIMEOUT): ?>
+		<h3>Giornata <?php echo GIORNATA; ?></h3>
+		
+		<div id="campo" class="column">
+			<div id="portiere" class="droppable" name="P"></div>
+			<div id="difensori" class="droppable" name="D"></div>
+			<div id="centrocampisti" class="droppable" name="C"></div>
+			<div id="attaccanti" class="droppable" name="A"></div>
+		</div>
+			<div id="giocatori" class="column" >
+			<?php foreach($this->giocatori as $key=>$val): ?>
+				<div id="<?php echo $val['idGioc'] ?>" name="<?php echo $val['ruolo']; ?>" class="draggable giocatore <?php echo $val['ruolo']; ?>">
+					<img width="40" src="imgs/foto/<?php echo $val['idGioc']; ?>.jpg" />
+					<?php echo $val['cognome'] . ' ' . $val['nome']; ?>
+				</div>
+			<?php $j++; endforeach; ?>
+			</div>
+			<div id="field">
+	<?php for($i=0;$i<11;$i++): ?>
+		<input <?php if(isset($this->titolari[$i])) echo 'value="' . $this->titolari[$i] . '" title="' . $this->giocatori[$this->titolari[$i]]['cognome'] . ' ' . $this->giocatori[$this->titolari[$i]]['nome'] . '" rel="' . $this->giocatori[$this->titolari[$i]]['ruolo'] . '"' ?> id="gioc-<?php echo $i; ?>" type="hidden" name="gioc[<?php echo $i; ?>]" />
+	<?php endfor; ?>
+	</div>
+	<?php endif; ?>
+	<script type="text/javascript">
+  $(document).ready(function(){
+  		list = $("#field").find("input");
+			list.each(function (i) {
+				$("#campo div.droppable[name=" +$(list[i]).attr('rel')+"]").append('<div id="'+ $(list[i]).attr('value') +'" name="'+ $(list[i]).attr('rel') +'" class="embed giocatore draggable ui-draggable '+ $(list[i]).attr('rel') +'"><img width="40" src="imgs/foto/' + $(list[i]).attr('value') + '.jpg" /><p>' + $(list[i]).attr('title') + '</p></div>');
+			});
+		$(".draggable").draggable({
+			helper:"clone",opacity:0.5,revert:true
+		});
+		var data = new Array();
+				data['P']=1;
+				data['D']=5;
+				data['C']=5;
+				data['A']=3;
+		$('.droppable').droppable({
+			accept: function(draggable) {
+				var nome = $(this).attr('name');
+				if($(draggable).attr('name') == nome) {
+				var nPor = 0;
+				var nDif = 0;
+				var nCen = 0;
+				var nAtt = 0;
+				var nTot = 0;
+				var n = 0;
+				
+				$(this).find("div").each(function () {
+					n++;
+				});
+				$(this).parent().find("div.embed").each(function () {
+					nTot++;
+				});
+				$(this).parent().find("div.P").each(function () {
+					nPor++;
+				});
+				$(this).parent().find("div.D").each(function () {
+					nDif++;
+				});
+				$(this).parent().find("div.C").each(function () {
+					nCen++;
+				});
+				$(this).parent().find("div.A").each(function () {
+					nAtt++;
+				});
+					if(nDif <= 2)
+					{ 
+						data['D'] = 5;
+						data['C'] = 5;
+						data['A'] = 3;
+						if(nCen == 4)
+						{
+							if(nAtt == 3)
+							{
+								data['D'] = 3;
+								data['C'] = 4;
+								data['A'] = 3;
+							}
+								
+						}
+					}
+					if(nDif == 3)
+					{ 
+						data['D'] = 5;
+						data['C'] = 5;
+						data['A'] = 3;
+						if(nCen == 5)
+						{
+							data['D'] = 4;
+							data['C'] = 5;
+							data['A'] = 2;
+							if(nAtt == 2)
+							{
+								data['D'] = 3;
+								data['C'] = 5;
+								data['A'] = 2;
+							}
+								
+						}
+						if(nAtt == 3)
+						{
+							data['D'] = 4;
+							data['C'] = 4;
+							data['A'] = 3;
+							if(nCen == 4)
+							{
+								data['D'] = 3;
+								data['C'] = 4;
+								data['A'] = 3;
+							}
+						}
+					}
+					if(nDif == 4)
+					{
+						data['D'] = 5;
+						data['C'] = 5;
+						data['A'] = 3;
+						if(nCen == 5)
+						{
+							data['D'] = 4;
+							data['C'] = 5;
+							data['A'] = 1;
+						}
+						else 
+						{
+							if(nCen == 4)
+							{
+								data['D'] = 5;
+								data['C'] = 5;
+								data['A'] = 2;
+								if(nAtt == 2)
+								{
+									data['D'] = 4;
+									data['C'] = 4;
+									data['A'] = 2;
+								}
+							}
+							else
+							{
+								if(nCen <= 3)
+								{
+									data['D'] = 5;
+									data['C'] = 5;
+									data['A'] = 3;
+									if(nAtt == 3)
+									{
+										data['D'] = 4;
+										data['C'] = 3;
+										data['A'] = 3;
+									}
+								}
+							}
+						}
+					}
+					if(nDif == 5)
+					{ 
+						data['D'] = 5;
+						data['C'] = 4;
+						data['A'] = 2;
+						if(nCen == 4)
+						{
+							data['D'] = 5;
+							data['C'] = 4;
+							data['A'] = 1;
+						}
+						else
+						{
+							data['D'] = 5;
+							data['C'] = 3;
+							data['A'] = 2;
+						}
+					}
+				
+				$("#pp").empty();
+				$("#pp").append(nPor);
+				$("#dd").empty();
+				$("#dd").append(nDif);
+				$("#cc").empty();
+				$("#cc").append(nCen);
+				$("#aa").empty();
+				$("#aa").append(nAtt);
+				$("#tt").empty();
+				$("#tt").append(nTot);
+				
+				
+				
+				
+				var numMax = data[($(this).attr('name'))];
+				$("#nn").empty();
+				$("#nn").append(numMax);
+				if(n < numMax && nTot < 11 && $(draggable).attr('name') == nome || $(draggable).is('.embed'))
+					return true;
+				}
+			},
+			activeClass: 'droppable-active',
+			hoverClass: 'droppable-hover',
+			/*activate:  function(draggable) { $('.embed').droppable('disable'); },
+			deactivate:  function(draggable) { $('.embed').droppable('enable'); },
+			over:function(draggable) { $('.embed').droppable('enable'); },
+			greedy: false,*/
+			drop: function(ev,ui) {
+						$(this).append('<div id="'+ui.draggable.attr('id') +'" name="'+ ui.draggable.attr('name') +'" style="'+ ui.helper.attr('style') +'" class="embed '+ui.draggable.attr('class')+'"><img width="40" src="imgs/foto/' + ui.draggable.attr('id') + '.jpg" /><p>' + $(ui.draggable).text() + '</p></div>');
+						$(this).children('div').css('opacity','1');
+						if((ui.draggable).parent().attr('id') == 'giocatori')
+							$(ui.draggable).addClass('hidden');
+						else
+							$(ui.draggable).remove();
+						$(ui.helper).remove();
+						list = $("#field").find("input");
+						list.each(function (i) {
+							$(list[i]).removeAttr('value');
+						});
+						lista = $("#campo").find("div.embed");
+						lista.each(function (i) {
+							$("#field #gioc-" + i).attr('value',$(lista[i]).attr('id'));
+						});
+						$(".draggable").draggable({
+							helper:"clone",opacity:0.5,revert:true
+						});
+						
+				}
+			});
+			$('#giocatori').droppable({
+			activeClass: 'droppable-active',
+			hoverClass: 'droppable-hover',
+			/*activate:  function(draggable) { $('.embed').droppable('disable'); },
+			deactivate:  function(draggable) { $('.embed').droppable('enable'); },
+			over:function(draggable) { $('.embed').droppable('enable'); },
+			greedy: false,*/
+			drop: function(ev,ui) {
+						$(this).append('<div id="'+ui.draggable.attr('id') +'" name="'+ ui.draggable.attr('name') +'" class="embed '+ui.draggable.attr('class')+'"><img width="40" src="imgs/foto/' + ui.draggable.attr('id') + '.jpg" /><p>' + $(ui.draggable).text() + '</p></div>');
+						$(this).children('div').css('opacity','1');
+						if((ui.draggable).parent().attr('id') == 'giocatori')
+							$(ui.draggable).addClass('hidden');
+						else
+							$(ui.draggable).remove();
+						$(ui.helper).remove();
+						list = $("#field").find("input");
+						list.each(function (i) {
+							$(list[i]).removeAttr('value');
+						});
+						lista = $("#campo").find("div.embed");
+						lista.each(function (i) {
+							$("input[name=gioc[" + i + "]]").attr('value',$(lista[i]).attr('id'));
+						});
+						$(".draggable").draggable({
+							helper:"clone",opacity:0.5,revert:true
+						});
+						
+				}
+			});
+		});
+	</script>
+</div>
+<div id="squadradett" class="column last">
+	<div class="box2-top-sx column last">
+	<div class="box2-top-dx column last">
+	<div class="box2-bottom-sx column last">
+	<div class="box2-bottom-dx column last">
+	<div class="box-content column last">
+		<?php if(!$this->timeout): //NON PIÙ NECESSARIO QUESTO IF PER L'HEADER LOCATION NELLA CODE' ?>
+		<div class="messaggio neut column last" >
+			<img alt="!" src="<?php echo IMGSURL.'attention-big.png'; ?>" />
+			<span>Non puoi effettuare operazioni in questo momento.Aspetta la fine delle partite</span>
+		</div>
+		<?php endif; ?>
+		<?php if($this->err == 1 || $this->err == 3): /*VUOL DIRE CHE C'È UN DOPPIONE O FORMAZIONE NN COMPLETA */ ?>
+			<div class="messaggio bad column last">
+				<img alt="!" src="<?php echo IMGSURL.'attention-bad-big.png'; ?>" title="Attenzione!" />
+				<span><?php if($this->err == 1): ?>Hai inserito dei valori multipli. <?php else: ?>Mancano dei valori. <?php endif; ?>La formazione non è stata modificata</span>
+			</div>
+		<?php elseif($this->err == 2): /* TUTTO OK */?>
+			<div class="messaggio good column last">
+					<img alt="OK" src="<?php echo IMGSURL.'ok-big.png'; ?>" />
+					<span>Operazione effettuata con successo</span>
+			</div>
+		<?php elseif($this->issetForm != FALSE):  ?>
+			<div class="messaggio neut column last">
+				<img alt="!" src="<?php echo IMGSURL.'attention-big.png'; ?>" title="Attenzione!" />
+				<span>Hai già impostato la formazione. Se la rinvii quella vecchia verrà sovrascritta</span>
+			</div>
+		<?php endif; ?>
+		<?php if($this->issetForm != FALSE || isset($this->err)): ?> 
+		<script type="text/javascript">
+		$(document).ready(function() {$('.messaggio').show('pulsate',{times: 3 }); });
+		$(".messaggio").click(function () {
+			$("div.messaggio").fadeOut("slow");
+		});
+		</script>
+		<?php endif; ?>
+		<?php if($_SESSION['logged'] == TRUE): ?>
+			<?php require (TPLDIR.'operazioni.tpl.php'); ?>
+		<?php endif; ?>
+		<form class="column last" name="form_modulo" action="index.php?p=formazione" method="post">
+			<fieldset id="modulo" class="no-margin fieldset">
+				<h3 class="no-margin">Seleziona il modulo:</h3>
+				<select name="mod" onchange="document.form_modulo.submit();">
+					<option></option>
+					<option value="1-4-4-2" <?php if ($this->value == '1-4-4-2') echo "selected=\"selected\""?>>4-4-2</option>
+					<option value="1-3-5-2" <?php if ($this->value == '1-3-5-2') echo "selected=\"selected\""?>>3-5-2</option>
+					<option value="1-3-4-3" <?php if ($this->value == '1-3-4-3') echo "selected=\"selected\""?>>3-4-3</option>
+					<option value="1-4-5-1" <?php if ($this->value == '1-4-5-1') echo "selected=\"selected\""?>>4-5-1</option>
+					<option value="1-4-3-3" <?php if ($this->value == '1-4-3-3') echo "selected=\"selected\""?>>4-3-3</option>
+					<option value="1-5-4-1" <?php if ($this->value == '1-5-4-1') echo "selected=\"selected\""?>>5-4-1</option>
+					<option value="1-5-3-2" <?php if ($this->value == '1-5-3-2') echo "selected=\"selected\""?>>5-3-2</option>
+				</select>
+			</fieldset>
+		</form>
+		<form class="right last" name="formazione_other" action="index.php?p=formazioniAll" method="post">
+			<fieldset class="no-margin fieldset">
+				<h3 class="no-margin">Guarda le altre formazioni</h3>
+				<?php if(empty($this->formazioniImpostate)): ?>
+					<select name="squadra" disabled="disabled">
+						<option>Nessuna form. impostata</option>
+				<?php else:?>
+					<select name="squadra" onchange="document.formazione_other.submit();">
+						<option></option>
+					<?php foreach($this->formazioniImpostate as $key=>$val): ?>
+						<option <?php if($this->squadra == $val[0]) echo "selected=\"selected\"" ?> value="<?php echo $val[0]?>"><?php echo $val[1]?></option>
+					<?php endforeach;?>
+				<?php endif; ?>
+				</select>
+			</fieldset>
+		</form>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+</div>
