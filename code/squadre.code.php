493107d044955bc76b67313df304d63ee3b41c71
Fiste788 <stefano788@gmail.com>
2009-11-02 18:47:13 +0000

    -nuova grafica pagina dettaglio squadre
    git-svn-id: http://subversion.assembla.com/svn/fantamanajer/branches/2.5@247 37f4913f-0a8a-4499-b109-c3fdd495100e

--- code/squadre.code.php
+++ code/squadre.code.php
@@ -1,20 +1,21 @@
 <?php
 require_once(INCDIR.'utente.inc.php');
 require_once(INCDIR.'punteggi.inc.php');
-require_once(INCDIR.'punteggi.inc.php');
 
 $punteggiObj = new punteggi();
 $utenteObj = new utente();
-$punteggiObj = new punteggi();
 
 $squadra = NULL;
 if(isset($_GET['squadra']))
 	$squadra = $_GET['squadra'];
-$contenttpl->assign('squadra',$squadra);
 
-$contenttpl->assign('posizioni',$punteggiObj->getPosClassifica($_SESSION['legaView']));
 $elencoSquadre = $utenteObj->getElencoSquadreByLega($_SESSION['legaView']);
+foreach ($elencoSquadre as $key => $val)
+	$elencoSquadre[$key]['giornateVinte'] = $punteggiObj->getGiornateVinte($val['idUtente']);
+
+$contenttpl->assign('squadra',$squadra);
 $contenttpl->assign('elencosquadre',$elencoSquadre);
+$contenttpl->assign('posizioni',$punteggiObj->getPosClassifica($_SESSION['legaView']));
 $contenttpl->assign('ultimaGiornata',$punteggiObj->getGiornateWithPunt());
 
 ?>
--- tpl/squadre.tpl.php
+++ tpl/squadre.tpl.php
@@ -7,18 +7,19 @@
 <div id="squadre" class="main-content">
 <?php foreach($this->elencosquadre as $key => $val): ?>
 	<div class="box-squadra column last">
+	<?php if(file_exists(UPLOADDIR. $val['idUtente'].'-small.jpg')): ?>
+		<a rel="group" title="<?php echo $val['nome']; ?>" class="fancybox" href="<?php echo UPLOADIMGURL.$val['idUtente'].'-original.jpg'; ?>" >
+			<img class="logo" alt="<?php echo $val['idUtente']; ?>" src="<?php echo UPLOADIMGURL. $val['idUtente'].'-med.jpg'; ?>" title="Clicca per vedere l'immagine ingrandita" />
+			<img class="logo reflex" alt="<?php echo $val['idUtente']; ?>" src="<?php echo UPLOADIMGURL. $val['idUtente'].'-med-reflex.jpg'; ?>" title="Clicca per vedere l'immagine ingrandita" />
+		</a>
+	<?php else: ?>
+		<img class="logo column last" alt="<?php echo $val['idUtente']; ?>" src="<?php echo IMGSURL.'no-foto.png'; ?>" title="<?php echo $val['nome']; ?>" />
+	<?php endif; ?>
+		<h3><a href="<?php echo $this->linksObj->getLink('dettaglioSquadra',array('squadra'=>$val['idUtente'])); ?>" title="Maggiori informazioni"><?php echo $val['nome']; ?></a></h3>	
 		<div class="column data">
-		<?php if(file_exists(UPLOADDIR. $val['idUtente'].'-small.jpg')): ?>
-			<a rel="group" title="<?php echo $val['nome']; ?>" class="fancybox" href="<?php echo UPLOADIMGURL.$val['idUtente'].'-original.jpg'; ?>" >
-				<img class="logo" alt="<?php echo $val['idUtente']; ?>" src="<?php echo UPLOADIMGURL. $val['idUtente'].'-med.jpg'; ?>" title="Clicca per vedere l'immagine ingrandita" />
-				<img class="logo reflex" alt="<?php echo $val['idUtente']; ?>" src="<?php echo UPLOADIMGURL. $val['idUtente'].'-med-reflex.jpg'; ?>" title="Clicca per vedere l'immagine ingrandita" />
-			</a>
-		<?php else: ?>
-			<img class="logo column last" alt="<?php echo $val['idUtente']; ?>" src="<?php echo IMGSURL.'no-foto.png'; ?>" title="<?php echo $val['nome']; ?>" />
-		<?php endif; ?>
-			<h3><a href="<?php echo $this->linksObj->getLink('dettaglioSquadra',array('squadra'=>$val['idUtente'])); ?>" title="Maggiori informazioni"><?php echo $val['nome']; ?></a></h3>	
-			<div>Proprietario: <?php echo $val['username'] ?></div>
-			<div>Pos. in classifica: <?php echo $this->posizioni[$val['idUtente']] ?></div>
+			<div>Proprietario: <?php echo $val['username']; ?></div>
+			<div>Pos. in classifica: <?php echo $this->posizioni[$val['idUtente']]; ?></div>
+			<div>Giornate vinte: <?php echo $val['giornateVinte']; ?></div>
 		</div>
 		<ul class="column link">
 			<li>
