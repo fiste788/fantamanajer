80dda07cd793512d21f5cccd66bec5cdb2119cfc
Stefano Sonzogni <stefano788@gmail.com>
2012-09-24 20:59:08 +0200

    -correzione errore dovuto a spostamento bootstrap

--- code/less2css.code.php
+++ code/less2css.code.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 require_once(INCDIR . 'lessc.inc.php');
 
 foreach($generalCss as $key=>$val) {
+    $file = strpos($val, "/") ? substr($val, strpos($val, "/") + 1) : $val;
     $less_fname = LESSDIR . $val . ".less";
-	$css_fname = CSSDIR . $val . ".css";
-	$cache_fname = CACHEDIR . $val . ".cache";
-	if (file_exists($cache_fname)) {
+	$css_fname = CSSDIR . $file . ".css";
+	$cache_fname = CACHEDIR . $file . ".cache";
+    $cache = "";
+	if (file_exists($cache_fname))
 		$cache = unserialize(file_get_contents($cache_fname));
-	} else {
+	else
 		$cache = $less_fname;
-	}
     $new_cache = lessc::cexecute($cache);
 	if (!is_array($cache) || $new_cache['updated'] > $cache['updated']) {
 		file_put_contents($cache_fname, serialize($new_cache));
 		file_put_contents($css_fname, $new_cache['compiled']);
 	}
-	lessc::ccompile(CSSDIR . 'less/' . $val . '.less', CSSDIR . $val . '.css');
-	$generalCss[$key] = $val . '.css';
+	lessc::ccompile(CSSDIR . 'less/' . $val . '.less', CSSDIR . $file . '.css');
+	$generalCss[$key] = $file . '.css';
 }
 ?>
