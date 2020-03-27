284a571808f7ee400f53e80978a018e9831d63af
Stefano Sonzogni <stefano788@gmail.com>
2020-03-27 18:59:46 +0100

    fixes

--- src/Command/ConsoleCommand.php
+++ src/Command/ConsoleCommand.php
@@ -34,6 +34,7 @@ class ConsoleCommand extends Command
      *
      * @param \Cake\Console\Arguments $args The command arguments.
      * @param \Cake\Console\ConsoleIo $io The console io
+     * @throws \Exception
      *
      * @return int|null The exit code or null for success
      */
